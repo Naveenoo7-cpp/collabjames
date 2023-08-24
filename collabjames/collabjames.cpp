7ffdc94fb1c3c5cac129ba936efea5aae77a4f21
Naveenoo7-cpp <142372310+Naveenoo7-cpp@users.noreply.github.com>
2023-08-24 08:55:30 +0530

    changed full code

--- collabjames/collabjames.cpp
+++ collabjames/collabjames.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
+void fun1(int a, int b);
+void fun2(int c);
+void fun1(int a, int b) {
+    int c = a + b;
+    fun2(c);
+}
+void fun2(int c) {
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << "Hello bro\n";
+    cout << c << endl;
+}
+
+int main() {
+    int a, b;
+    cout << "Enter the first number:";
+    cin >> a >> b;
+    fun1(a, b);
+    return 0;
 }
